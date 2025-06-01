f66b77e51cd9a47dfed5924e89b2bdff72f5ed3d
ShivamCo <shivam.singh.coo@gmail.com>
2025-06-01 16:21:12 +0530

    Bug fixes for deployment

--- src/app/api/photographers/route.js
+++ src/app/api/photographers/route.js
@@ -18,29 +18,4 @@ export async function GET() {
 
 
 
-export default async function handler(req, res) {
-  const {
-    query: { id },
-    method,
-  } = req;
 
-  if (method !== 'GET') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-
-  try {
-    const filePath = path.join(process.cwd(), 'src', 'database', 'db.json');
-    const jsonData = await fs.readFile(filePath, 'utf-8');
-    const data = JSON.parse(jsonData);
-
-    const photographer = data.photographers.find((p) => p.id === id);
-
-    if (!photographer) {
-      return res.status(404).json({ message: 'Photographer not found' });
-    }
-
-    return res.status(200).json(photographer);
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal Server Error', error: error.message });
-  }
-}
